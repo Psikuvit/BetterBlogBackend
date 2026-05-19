3064a76a2fb1f4b86f112074133318b5193c5059
Psikuvit <thegengestar@gmail.com>
2026-05-20 00:45:27 +0100

    Feature: initialize BetterBlog application with Maven setup and configuration files

---
+++ src/main/java/me/psikuvit/betterblog/BetterBlogApplication.java
@@ -0,0 +1,13 @@
+package me.psikuvit.betterblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/psikuvit/betterblog/BetterBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.psikuvit.betterblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
