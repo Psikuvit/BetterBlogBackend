2379a30bafe427b3779ca4e5e4040a6f9cd5c0a1
Psikuvit <thegengestar@gmail.com>
2026-05-20 00:46:14 +0100

    Feature: add custom exception classes for better error handling

---
+++ src/main/java/me/psikuvit/betterblog/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package me.psikuvit.betterblog.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
