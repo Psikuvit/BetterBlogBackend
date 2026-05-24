d33a63db3bf04653462648764f2f47f4ca1d1624
Psikuvit <thegengestar@gmail.com>
2026-05-24 18:07:34 +0100

    Feature: add DTOs for password reset functionality

---
+++ src/main/java/me/psikuvit/betterblog/dto/ResetPasswordRequest.java
@@ -0,0 +1,28 @@
+package me.psikuvit.betterblog.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResetPasswordRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Reset code is required")
+    private String code;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "New password must be at least 8 characters")
+    private String newPassword;
+}
+
---
+++ src/main/java/me/psikuvit/betterblog/dto/ResetPasswordResponse.java
@@ -0,0 +1,15 @@
+package me.psikuvit.betterblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResetPasswordResponse {
+    private String message;
+}
+
